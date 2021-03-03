53f740bb2b96ee5fe4227b034cf79139f3de3ef5
Parker Brandt <parker.brandt101@gmail.com>
2021-03-03 23:55:11 +0000

    Created first test code to make sure the code can read and write characters properly

--- project0.c
+++ project0.c
@@ -1,8 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_SIZE 5
+
+// Structs
+struct HashMap
+{
+	char key;
+	int value;
+};
+
+// Prototype Functions
+void printOutput(int* array);
+
+
+// Main Function
 int main(int argc, char *argv[])
 {
-	
+	// Variables
+	char buffer[MAX_SIZE];
+	int count = 0;
+	char ch;
+	struct HashMap *map;	
+
+	while((ch = getchar()) != EOF)
+	{
+		buffer[count] = ch;
+
+		if(ch == ' ')
+		{
+			
+			count = 0;
+		}
+		else
+		{
+			//
+			
+			count++;
+		}
+
+		printf("Char: ");
+		putchar(ch);
+		printf("\n");
+	}
+
 	return 0;
 }
